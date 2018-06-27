fccd92280f88385223d2f23052ffadefb2d4bca3
kjy1111 <ekfto7@nate.com>
2018-06-27 15:56:21 +0900

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\ProgramData_Yeon\PycharmProjects\python_ch3\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
