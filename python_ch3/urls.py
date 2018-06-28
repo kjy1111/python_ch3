5b9aa6bde75930bb13327d1b35c5b9ee0546c42a
kjy1111 <ekfto7@nate.com>
2018-06-28 10:30:46 +0900

    Initial commit

--- python_ch3/urls.py
+++ python_ch3/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('emaillist/form', emaillist_views.form),
     path('emaillist/add', emaillist_views.add),
 
-    path('guestbook/', guestbook_views.index)
+    path('guestbook/', guestbook_views.index),
+    path('guestbook/add', guestbook_views.add)
 ]
