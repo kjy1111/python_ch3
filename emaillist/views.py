5b6e11cd4c93cc662f43e28e620892ee316b0c0e
kjy1111 <ekfto7@nate.com>
2018-06-28 09:47:19 +0900

    Initial commit

--- emaillist/views.py
+++ emaillist/views.py
@@ -8,11 +8,11 @@ from django.http import HttpResponseRedirect
 def index(request):
     emaillist_list = Emaillist.objects.all().order_by('-id')
     data = {'emaillist_list': emaillist_list}
-    return render(request, 'index.html', data)
+    return render(request, 'emaillist/index.html', data)
 
 
 def form(request):
-    return render(request, 'form.html')
+    return render(request, 'emaillist/form.html')
 
 
 def add(request):
